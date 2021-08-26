3784207b931665ac1f80dc190a615766a05efcd0
Nachtalb <info@nachtalb.io>
2021-08-26 21:47:35 +0200

    Release 1.2.0

--- __init__.py
+++ __init__.py
@@ -91,7 +91,7 @@ class AutoBuilder(StopableThread):
 
 class Plugin(BasePlugin):
 
-    __name__ = 'Upload Statistics DEV'
+    __name__ = 'Upload Statistics'
     settings = {
         'stats_file': str(BASE_PATH / 'stats.json'),
         'stats_html_file': str(BASE_PATH / 'index.html'),
@@ -426,6 +426,6 @@ Only files that have been uploaded more than this will be shown on the statistic
         return returncode['zap']
 
     __privatecommands__ = __publiccommands__ = [
-        ('dupstats', open_stats),
-        ('dupstats-reset', reset_stats)
+        ('upstats', open_stats),
+        ('upstats-reset', reset_stats)
     ]
